3ed3a429da4632632904373df3b7bf29ded3d183
ranjanpiyush1910 <36705480+ranjanpiyush1910@users.noreply.github.com>
2018-02-27 22:59:21 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,6 +9,10 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+app.get('/', function (req, res) {
+    res.sendFile('Article one requested & will be served here');
+});
+
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
