fcc794c494f03c056ef13e5857bac6c6eb0afbd6
ranjanpiyush1910 <36705480+ranjanpiyush1910@users.noreply.github.com>
2018-02-28 11:57:19 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -7,7 +7,7 @@ element.innerHTML = 'New value';
 var img = document.getElementById('madi');
 marginLeft = 0;
 function moveRight () {
-    marginLeft = marginLeft + 10;
+    marginLeft = marginLeft + 1;
     img.style.marginLeft = marginLeft + 'px';
 }
 img.onclick= function () {
