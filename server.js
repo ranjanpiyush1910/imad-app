f4d73fc0968c24bbb09b29bcc0a2763aa0dd67c2
ranjanpiyush1910 <36705480+ranjanpiyush1910@users.noreply.github.com>
2018-03-02 16:56:41 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,6 +1,15 @@
 var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
+var Pool = require('pg').Pool;
+
+var config = {
+    user: 'ranjanpiyush1910',
+    database: 'ranjanpiyush1910',
+    host: 'db.imad.hasura-app.io',
+    port: '5432',
+    password: process.env.DB_PASSWORD
+};
 
 var app = express();
 app.use(morgan('combined'));
@@ -91,8 +100,21 @@ function createTemplate (data) {
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
-var counter = 0;
 
+var pool = new Pool(config);
+app.get('/test-db', function(req, res) {
+  //make a request 
+  //return the response with the results 
+  pool.query('SELECT * FROM test', function(err, result) {
+    if(err) {
+     res.status(500).send(err.toString());   
+    } else {
+        res.send(JSON.stringify(result));
+    }
+  });
+});
+
+var counter = 0;
 app.get('/counter', function (req, res) {
     counter = counter + 1;
     res.send(counter.toString());
